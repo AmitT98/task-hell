7770493a24a6ff7722b2158742ce49f418d25d09
Amit Tiwari <amit@Amits-MacBook-Air.local>
2023-07-09 22:50:43 +0530

    remove gitignore files

--- loadTask.js
+++
@@ -1,8 +0,0 @@
-export function saveTask() {
-    
-    const ptag = document.createElement("p")
-    const currentTask = document.getElementById("ip1").value
-    ptag.innerHTML= currentTask
-    document.getElementById("tasks").appendChild(ptag)
-  }
-  
