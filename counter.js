796854e26a62384aff556b04e82cd8f9a7b2a5ac
AmitT98 <amitlives2explore@gmail.com>
2023-07-10 22:06:59 +0530

    added the ability to save tasks through cookies

--- counter.js
+++ counter.js
@@ -3,10 +3,10 @@ import { saveTask } from "./saveTask"
 export function setupCounter(element) {
   let counter = 0
   const setCounter = (count) => {
-    saveTask()
-    document.getElementById("ip1").value = ""
     counter = count
     element.innerHTML = `Current tasks: ${counter}`
+    saveTask()
+    document.getElementById("ip1").value = ""
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
