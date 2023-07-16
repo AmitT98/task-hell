dff3a2ab83132c57e11cc3a50b692d97d6af9c7a
AmitT98 <amitlives2explore@gmail.com>
2023-07-16 22:02:19 +0530

    minor bug fix

--- c2s.js
+++ c2s.js
@@ -1,9 +1,13 @@
 export function c2s(evt) {
-    const duration  = Number(evt.target.getAttribute("data-duration"))/1000
+    const checkStart = Number(evt.target.getAttribute("data-startTime"));
+    if(checkStart == 0){
+    const duration  = Number(evt.target.getAttribute("data-duration"))
     const startTime  = new Date().getTime()/1000;//Number(evt.target.getAttribute("data-startTime"))/1000;
     const currTime = new Date().getTime()/1000;
     evt.target.style.color = "rgb(" + "0".toString() + " "+ "255".toString() + " 0)"
     evt.target.setAttribute("data-startTime",startTime);
     //evt.target.setAttribute("data-duration",startTime+duration*1000);  
     //console.log("I was here")
+    document.cookie =    evt.target.innerHTML.split(":")[0] + "= " + evt.target.innerHTML.split(":")[1] +  "::" + duration + "::" + startTime +  ";" + "expires=Tue, 19 Jan 2038 04:14:07 GMT"
+    }
 }
