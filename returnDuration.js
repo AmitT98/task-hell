469091bc379fce660c04880aafcd2be4f7be5284
AmitT98 <amitlives2explore@gmail.com>
2023-07-21 22:40:51 +0530

    tasks can now be added in -h-m-s format

--- returnDuration.js
+++ returnDuration.js
@@ -5,10 +5,15 @@ export function returnDuration(){
         duration = Number(ip3.toLowerCase().split("h")[0])*3600;
     }
     else if(ip3[ip3.length-1].toLowerCase() == 'm'){
-        duration = Number(ip3.toLowerCase().split("m")[0])*60;
+        duration += Number(ip3.toLowerCase().split("m")[0].split("h")[0])*3600
+        duration += Number(ip3.toLowerCase().split("m")[0].split("h")[1])*60;
     }
     else{
-        duration = Number(ip3.toLowerCase().split("s")[0])
+        let ip3m = ip3.toLowerCase().split("m").length == 1?("0m"+ip3):ip3
+        ip3m = ip3m.toLowerCase().split("h").length == 1?("0h" + ip3m):ip3m;
+        duration += Number((ip3m).toLowerCase().split("h")[0])*3600
+        duration += Number((ip3m).toLowerCase().split("h")[1].split("m")[0])*60
+        duration += Number((ip3m).toLowerCase().split("s")[0].split("m")[1])
     }
     duration = isNaN(duration)?1:duration;
     console.log(duration)
