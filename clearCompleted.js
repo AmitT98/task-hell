8c8079aa92d35a24d428cb5646fc9826b163cf7d
AmitT98 <amitlives2explore@gmail.com>
2023-07-24 20:56:02 +0530

    Fixed a minor bug on clear completed tasks

--- clearCompleted.js
+++ clearCompleted.js
@@ -2,9 +2,9 @@ export function clearCompleted(evt){
     const length = document.getElementById("completed").childNodes.length;
     console.log(length)
     for(let i = 2; i < length;i++){
-        const taskName = document.getElementById("completed").childNodes[i].innerHTML 
+        const taskName = document.getElementById("completed").childNodes[2].innerHTML 
         document.cookie = taskName +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT'
-        document.getElementById("completed").childNodes[i].remove();
+        document.getElementById("completed").childNodes[2].remove();
     }
     
 }
--- click2start.js
+++ click2start.js
@@ -6,7 +6,7 @@ export function click2start(evt) {
     const currTime = new Date().getTime()/1000;
     evt.target.style.color = "rgb(" + "0".toString() + " "+ "255".toString() + " 0)"
     evt.target.setAttribute("data-startTime",startTime);
-    evt.target.setAttribute("title", new Date((startTime+duration)*1000))
+    evt.target.setAttribute("title", new Date((startTime+duration)*1000).toString().slice(4,24))
     //evt.target.setAttribute("data-duration",startTime+duration*1000);  
     //console.log("I was here")
     document.cookie =    evt.target.innerHTML.split(":")[0] + "= " + evt.target.innerHTML.split(":")[1] +  "::" + duration + "::" + startTime +  ";" + "expires=Tue, 19 Jan 2038 04:14:07 GMT"
