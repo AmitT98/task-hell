0d6d2d0b43376315f5aee78d615a0a23c7399a16
AmitT98 <amitlives2explore@gmail.com>
2023-07-24 21:07:47 +0530

    Fixed expired loading into input box bug

--- renew.js
+++ renew.js
@@ -1,6 +1,8 @@
 export function renew(evt){
+    if(evt.target.innerHTML != "Expired🪦"){
     document.getElementById('ip1').value = evt.target.innerText
     document.getElementById('ip2').value = evt.target.getAttribute("title")
     document.getElementById('ip3').value = evt.target.getAttribute("data-duration")
     evt.target.remove()
+    }
 }
